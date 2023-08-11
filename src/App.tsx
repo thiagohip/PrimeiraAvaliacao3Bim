7097a638aa36e7c6a919d19d57a178d9b4c95af1
thiagohip <thiaggohipolitomoreira@gmail.com>
2023-08-11 09:59:44 -0300

    foi

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { HomePage} from "./pages";
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle/>
+      <HomePage/>
+      
+    </>
+  )
+}
+export default App
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    border: 0;
+    box-sizing: border-box;
+
+}
+
+ body{
+    background: rgb(204,88,88);
+    background: linear-gradient(216deg, rgba(204,88,88,1) 0%, rgba(112,45,45,1) 100%);
+ }
+`
